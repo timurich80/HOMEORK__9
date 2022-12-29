d9a3ae22ac9c8501e3390b1b2aa8210f11eb385f
timurich80 <timurich80@gmail.com>
2022-12-29 22:00:13 +0300

    Все работает

---
+++ task_64/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 64: Задайте значения M и N. Напишите программу, 
+которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+M = 1; N = 5 -> 2, 4
+M = 4; N = 8 -> 4, 6, 8 */
+
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+NaturalToLow(n, m);
+void NaturalToLow(int n, int m)
+{
+    if (m > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, m + 1);
+        Console.Write(m + " ");
+    }
+}
---
+++ task_68/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 68: Задайте значения M и N. Напишите программу, 
+которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+M = 28; N = 7 -> 7 */
+System.Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+// NOD (a,b);
+PrintResult(a,b);
+void PrintResult(int m, int n)
+{
+    System.Console.WriteLine($"НОД -> {NOD (a,b)}");
+}
+
+int NOD (int a, int b)
+{
+        if (b == 0)
+                return a;
+        else
+                return NOD (b, a % b);
+}
+
+
+// void button1_Click(object sender, EventArgs e)
+//         {
+//            int a, b, c;
+//            a = Convert.ToInt32(textBox1.Text);
+//            b = Convert.ToInt32(textBox2.Text);
+//            textBox3.Text = nod(a, b).ToString();
+ 
+ 
+ 
+//         }
